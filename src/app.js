6eedb9d4a04352edff04d79e5b5ae51c4005f50d
Renato Morales <104334139+naatox@users.noreply.github.com>
2024-12-07 17:30:54 -0300

    refactor: folders

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const db = require("./database/database");
 
 const app = express();
 
 
-const careersRouter = require ("../routes/careersRoutes");
+const careersRouter = require ("./routes/careersRoutes");
+const subjectsRouter = require ("./routes/subjectsRoutes");
 
 
 app.use(express.json());
@@ -13,5 +15,6 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/careers", careersRouter);
+app.use("/subjects", subjectsRouter);
 
 module.exports = app; 
