d5037c2211436879212564b2e98b9ce719154531
Renato Morales <104334139+naatox@users.noreply.github.com>
2024-12-15 14:58:00 -0300

    delete: unnecesary code

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ const morgan = require("morgan");
 const db = require("./database/database");
 
 const startGrpcServer = require("./grpcClient");
-const RabbitService = require("./services/rabbitService");
-const careersRouter = require ("./routes/careersRoutes");
-const subjectsRouter = require ("./routes/subjectsRoutes");
 
 const app = express();
 
@@ -19,13 +16,9 @@ app.use(express.urlencoded({ extended: false }));
 // GRPC
 
 startGrpcServer();
-const rabbitService = new RabbitService();
-rabbitService.setupRabbitMQ();
 
 
-// HTTP
-app.use("/api/careers", careersRouter);
-//app.use("/api/subjects", subjectsRouter);
+
 
 
 
